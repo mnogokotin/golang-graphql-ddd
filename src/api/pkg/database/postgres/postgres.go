b823503efcadc18483a6543a238edf381e2bfed0
mnogokotin <mnogokotin@gmail.com>
2023-09-18 18:24:24 +0400

    postgres pkg fix

--- src/api/pkg/database/postgres/postgres.go
+++ src/api/pkg/database/postgres/postgres.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/mnogokotin/golang-graphql-ddd/graph/model"
 	"os"
 )
 
@@ -17,14 +16,4 @@ func getDatabaseUrl() string {
 func GetDatabase() (*gorm.DB, error) {
 	db, err := gorm.Open("postgres", getDatabaseUrl())
 	return db, err
-}
-
-func RunMigrations(db *gorm.DB) {
-	if !db.HasTable(&model.Question{}) {
-		db.CreateTable(&model.Question{})
-	}
-	if !db.HasTable(&model.Choice{}) {
-		db.CreateTable(&model.Choice{})
-		db.Model(&model.Choice{}).AddForeignKey("question_id", "questions(id)", "CASCADE", "CASCADE")
-	}
 }
